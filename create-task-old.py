14f1bf25487080f19dacc61433985e48c9892b32
peppapig450 <nbgenius1@gmail.com>
2024-03-04 18:12:17 -0800

    working

--- create-task-old.py
+++ create-task-old.py
@@ -1,24 +1,40 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_experimental_option("detach", True)
 
-driver.get('https://www.grailed.com')
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = "https://www.grailed.com"
 
-try:
-    wait = WebDriverWait(driver, 10)
-  #  accept_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#onetrust-accept-btn-handler")))
-#    accept_button.click()
-    accept_button = driver.find_element(By.CSS_SELECTOR, "#onetrust-accept-btn-selector")
-    driver.execute_script("arguments[0].click();", accept_button)
+driver.get(base_url)
 
-except:
-    print("Accept button not found.")
-    driver.quit()
+def get_to_search_bar_to_search(driver, timeout=5):
+    popup_locater = (By.CSS_SELECTOR, "div.Modal-Content > div.Modal-Body")
 
-#search_bar = driver.find_element(By.ID, "header_search-input")
-driver.implicitly_wait(5)
-#search_bar.send_keys("supreme belt")
-input()
+    try:
+        # click on the search bar first
+        search_bar = driver.find_element(By.CSS_SELECTOR, "#header_search-input")
+        search_bar.click()
+        wait = WebDriverWait(driver, timeout)
+        popup = wait.until(EC.presence_of_element_located(popup_locater))
+
+        ActionChains(driver)\
+            .send_keys(Keys.ESCAPE)\
+            .perform()
+
+        # check if popup is still there
+       # popup = driver.find_element(popup_locater)
+    except (NoSuchElementException, StaleElementReferenceException) as e:
+        print(f"Error interacting with element: {e}")
+    except TimeoutException:
+        print("Element not found within timeout!")
+
+get_to_search_bar_to_search(driver)
--- create-task.py
+++ create-task.py
@@ -3,38 +3,97 @@ from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    StaleElementReferenceException,
+    TimeoutException,
+    ElementClickInterceptedException,
+)
 from selenium.webdriver.common.by import By
 
-options = Options()
-options.add_experimental_option("detach", True)
+import sys
 
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = "https://www.grailed.com"
 
-driver.get(base_url)
+def dismiss_login_popup(driver, timeout=5):
+    try:
+        login_popup = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "ReactModal__Overlay ReactModal__Overlay--after-open modal-overlay"))
+        )
+
+       # ActionChains(driver).move_to_element(login_popup).move_by_offset(0, 300).click().perform()
+        script = """
+        var elementToRemove = document.querySelector('.ReactModalPortal');
+        elementToRemove && elementToRemove.remove();
+        """
+        driver.execute_script(script)
+
+        WebDriverWait(driver, timeout).until_not(
+            EC.presence_of_element_located((By.CLASS_NAME, "ReactModal__Overlay.ReactModal__Overlay--after-open.modal-overlay"))
+        )
+
+
+    except TimeoutException:
+        print("Login popup did not appear within the timeout.")
+
+
+def get_search_query():
+    if len(sys.argv > 1):
+        search_query = sys.argv[1]
+    else:
+        search_query = input("Enter your search query: ")
+
+    return search_query
+
+
+def accept_cookies(driver):
+    try:
+        cookies_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+        )
+        ActionChains(driver).double_click(cookies_button).perform()
+    except TimeoutException:
+        print("Timeout occured")
+
 
 def get_to_search_bar_to_search(driver, timeout=5):
-    popup_locater = (By.CSS_SELECTOR, "div.Modal-Content > div.Modal-Body")
 
     try:
-        # click on the search bar first
+        wait = WebDriverWait(driver, timeout)
+
+        accept_cookies(driver)
+
+        # driver.execute_script("document.querySelector('.ReactModal__Overlay').style.display = 'none';")
+        #  ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
         search_bar = driver.find_element(By.CSS_SELECTOR, "#header_search-input")
         search_bar.click()
-        wait = WebDriverWait(driver, timeout)
-        popup = wait.until(EC.presence_of_element_located(popup_locater))
 
-        ActionChains(driver)\
-            .send_keys(Keys.ESCAPE)\
-            .perform()
-
-        # check if popup is still there
-       # popup = driver.find_element(popup_locater)
+    # check if popup is still there
     except (NoSuchElementException, StaleElementReferenceException) as e:
         print(f"Error interacting with element: {e}")
+        driver.quit()
     except TimeoutException:
         print("Element not found within timeout!")
+        driver.quit()
 
-get_to_search_bar_to_search(driver)
+
+def type_search(search):
+    search_bar = driver.find_element(By.CSS_SELECTOR, "#header_search-input")
+    ActionChains(driver).send_keys_to_element(search_bar, search).perform()
+
+
+if __name__ == "__main__":
+    options = Options()
+    options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(
+        options=options, service=ChromeService(ChromeDriverManager().install())
+    )
+    base_url = "https://www.grailed.com"
+
+    driver.get(base_url)
+    get_to_search_bar_to_search(driver)
+    search_query = get_search_query()
