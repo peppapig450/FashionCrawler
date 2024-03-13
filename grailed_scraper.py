fa772a96cfd7be50637c7c54f9d3266bbf42c04f
peppapig450 <nbgenius1@gmail.com>
2024-03-12 17:28:30 -0700

    type pointer thingies, delete the modularization to dismiss the login popup as its considerebly slower for some reason

--- grailed_scraper.py
+++ grailed_scraper.py
@@ -23,11 +23,29 @@ class GrailedScraper(BaseScraper):
     def __init__(self, headless):
         super().__init__(headless)
 
-    def run_grailed_scraper(self, search_query):
+    def run_grailed_scraper(self, search_query: str) -> None:
+        """
+        Runs the Grailed scraper to search for items based on the provided search query.
+
+        Args:
+        - search_query (str): The search query to be used for searching items.
+
+        Returns:
+        - None
+        """
         self._navigate_and_search(search_query)
         super().wait_for_page_load(self.ITEM_CLASS_NAME, self.MIN_COUNT)
 
-    def _navigate_and_search(self, search_query):
+    def _navigate_and_search(self, search_query: str) -> None:
+        """
+        Navigates to the search bar and performs a search based on the provided query.
+
+        Args:
+        - search_query (str): The search query to be used for searching items.
+
+        Returns:
+        - None
+        """
         super().navigate_to_search_bar(self.BASE_URL, self.SEARCH_BAR_CSS_SELECTOR)
         super().search_for_query(
             search_query,
@@ -35,7 +53,9 @@ class GrailedScraper(BaseScraper):
             self.SEARCH_BAR_SUBMIT_CSS_SELECTOR,
         )
 
-    def get_to_search_bar_to_search(self, search_bar_css_selector, timeout=2):
+    def get_to_search_bar_to_search(
+        self, search_bar_css_selector: str, timeout=2
+    ) -> None:
         """
         Navigate to the search bar and interact with it to initiate a search.
 
@@ -56,7 +76,7 @@ class GrailedScraper(BaseScraper):
 
             for _ in range(3):
                 try:
-                    self.dismiss_login_popup(timeout=2)
+                    self._dismiss_login_popup(timeout=2)
                     break
                 except TimeoutException:
                     pass
@@ -69,64 +89,42 @@ class GrailedScraper(BaseScraper):
             print(f"Error interacting with search bar: {e}")
             self.driver.quit()
 
-    def dismiss_login_popup(self, timeout=5):
+    def _dismiss_login_popup(self, timeout: int) -> None:
         """
         Dismisses the login popup within a specified timeout period.
 
         Args:
-        - timeout: The maximum time to wait for the login popup.
+        - timeout (int): The maximum time to wait for the login popup.
 
         Returns:
         - None
         """
+
+        # TODO: dont modularize this for now it's slower for some reason
         try:
-            login_popup = self._wait_for_login_popup(timeout)
-            self._close_login_popup(login_popup)
-            self._wait_for_login_popup_to_disappear()
+            login_popup = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located(
+                    (
+                        By.CSS_SELECTOR,
+                        ".ReactModal__Content.ReactModal__Content--after-open.modal.Modal-module__authenticationModal___g7Ufu._hasHeader",
+                    )
+                )
+            )
+
+            ActionChains(self.driver).move_to_element(login_popup).pause(
+                1
+            ).move_by_offset(250, 0).pause(1).click()
+
+            ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+
+            WebDriverWait(self.driver, 10).until_not(
+                EC.presence_of_element_located(
+                    (
+                        By.CLASS_NAME,
+                        "ReactModal__Overlay.ReactModal__Overlay--after-open.modal-overlay",
+                    )
+                )
+            )
 
         except TimeoutException:
             print("Login popup did not appear within the timeout.")
-
-    def _wait_for_login_popup(self, timeout):
-        """
-        Wait for the login popup to appear.
-
-        Args:
-            timeout (int): The maximum time to wait for the login popup.
-
-        Returns:
-            WebElement: The login popup element.
-        """
-        return WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, self.LOGIN_POPUP_SELECTOR))
-        )
-
-    def _close_login_popup(self, login_popup):
-        """
-        Close the login popup.
-
-        Args:
-            login_popup (WebElement): The login popup element.
-
-        Returns:
-            None
-        """
-        ActionChains(self.driver).move_to_element(login_popup).pause(1).move_by_offset(
-            250, 0
-        ).pause(1).click().pause(1).send_keys(Keys.ESCAPE).perform()
-
-    def _wait_for_login_popup_to_disappear(self):
-        """
-        Wait for the login popup to disappear.
-
-        Returns:
-            None
-        """
-        return WebDriverWait(self.driver, 15).until_not(
-            EC.presence_of_element_located(
-                (
-                    By.CLASS_NAME,
-                    "ReactModal__Overlay.ReactModal__Overlay--after-open.modal-overlay",
-                )
-            )
-        )
