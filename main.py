cadfda1fc2fd3d3f218add60cd1a4c47f4d391fd
peppapig450 <nbgenius1@gmail.com>
2024-03-14 11:30:12 -0700

    change it back to the original method until i can figure out the issues and i dont really like the method anyway. + remove unncessary search import

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from scraper import DepopScraper, GrailedScraper
 from data_extraction import GrailedDataExtractor
 
 
+# FIXME: problems with saving the output still isnt really efficient
+# FIXME: also spawns some a random blank window
 def run_scraper(scraper, extractor, search_query, output_filename, config):
     """
     Run the specified scraper, extract data, and save it to a file.
@@ -31,17 +33,21 @@ def main():
     search_query = config.get("search_query", "")
 
     # TODO: figure out how we're gonna run each websites scraper
-    grailed_scraper = GrailedScraper(headless=config["headless"])
-    depop_scraper = DepopScraper(headless=config["headless"])
+    gscraper = GrailedScraper(headless=config["headless"])
+    dscraper = DepopScraper(headless=config["headless"])
 
-    # not perfect way to do this but its a start I guess.
-    run_scraper(
-        grailed_scraper,
-        GrailedDataExtractor(driver=grailed_scraper.driver),
-        search_query,
-        config.get("output", f"{search_query}_grailed"),
-        config,
-    )
+    try:
+        search_query = config.get("search_query", "")
+        gscraper.run_scraper(search_query)
+        # dscraper.run_scraper(search_query) test when i get home
+        extractor = GrailedDataExtractor(driver=gscraper.driver)
+        df = extractor.extract_data_to_dataframe()
+
+        output_filename = config.get("output", search_query)
+        IOUtils.save_output_to_file(df, output_filename, config)
+
+    finally:
+        gscraper.driver.quit()
 
 
 if __name__ == "__main__":
