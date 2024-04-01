a7576cf46f56eeea81a57c58c9a2e7f2c72586f1
peppapig450 <nbgenius1@gmail.com>
2024-03-31 17:21:27 -0700

    fix the top text of the license

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-# Copyright 2024 Nicholas Brady
+# Copyright 2024 Nicholas Brady. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
