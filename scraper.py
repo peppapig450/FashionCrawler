b15ccbeab4d1b40f730fb89a6b1ecb556a9b0cc7
peppapig450 <nbgenius1@gmail.com>
2024-04-02 20:46:24 -0700

    add base StockXScraper class

--- scraper.py
+++ scraper.py
@@ -566,3 +566,20 @@ class DepopScraper(BaseScraper):
             self.SEARCH_BAR_SELECTOR,
             self.SUBMIT_BUTTON_SELECTOR,
         )
+
+
+class StockxScraper(BaseScraper):
+    """
+    Subclass of BaseScraper for scraping data from the StockX website.
+    """
+
+    def __init__(self, base_scraper):
+        self.driver = base_scraper.driver
+
+    def get_to_search_bar_to_search(
+        self, search_bar_css_selector: str, timeout=2
+    ) -> None:
+        pass
+
+    def run_scraper(self, search_query):
+        pass
