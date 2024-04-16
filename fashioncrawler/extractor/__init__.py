28c6ae4384020ad0013678e55ae0afa72cd3c5c5
peppapig450 <nbgenius1@gmail.com>
2024-04-16 12:03:58 -0700

    module documetnation for the data extractor modules and package

--- fashioncrawler/extractor/__init__.py
+++ fashioncrawler/extractor/__init__.py
@@ -1,3 +1,15 @@
+"""
+Extractor Package
+=======================
+
+This package provides functionality for extracting data fashion-related websites.
+
+Modules:
+- base_data_extractor: Defines a base class for extracting data from web pages and storing it in a Pandas DataFrame.
+- grailed_data_extractor: Implements data extraction from the Grailed website.
+- depop_data_extractor: Implements data extraction from the Depop website.
+"""
+
 from .base_data_extractor import BaseDataExtractor
 from .grailed_data_extractor import GrailedDataExtractor
 from .depop_data_extractor import DepopDataExtractor
