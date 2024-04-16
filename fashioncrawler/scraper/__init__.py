f62911f454593360d6941f0cdcd0bd35bd8a8cc0
peppapig450 <nbgenius1@gmail.com>
2024-04-16 11:55:15 -0700

    module and package documetnation for the scrapers

--- fashioncrawler/scraper/__init__.py
+++ fashioncrawler/scraper/__init__.py
@@ -1,3 +1,16 @@
+"""
+Fashion Crawler Package
+=======================
+
+This package provides functionality for web scraping fashion-related websites.
+
+Modules:
+- base_scraper: Provides a base class with functionality for web scraping using Selenium.
+- grailed_scraper: Implements a scraper for the Grailed website.
+- depop_scraper: Implements a scraper for the Depop website.
+- stockx_scraper: Implements a scraper for the StockX website.
+"""
+
 from .base_scraper import BaseScraper
 from .grailed_scraper import GrailedScraper
 from .depop_scraper import DepopScraper
