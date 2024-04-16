99f7f04e762d8178192ac926af12e845ef67a5e3
peppapig450 <nbgenius1@gmail.com>
2024-04-16 12:12:02 -0700

    documetnation for overall fashion crawler package

--- fashioncrawler/__init__.py
+++ fashioncrawler/__init__.py
@@ -1,3 +1,21 @@
+"""
+Fashion Crawler Package
+=======================
+
+This package provides functionality for crawling and extracting data from fashion-related websites
+
+Subpackages:
+- scraper: Contains modules for scraping data from fashion-related websites.
+- extractor: Contains modules for extracting data from fashion-related websites.
+- utils: Contains miscellaneous utility functions.
+
+Modules:
+- scraper: Implements web scraping functionality for fashion-related websites.
+- extractor: Provides functionality for data extraction from fashion-related websites.
+- utils: Contains miscellaneous utility functions.
+
+"""
+
+from . import scraper
 from . import extractor
 from . import utils
-from . import scraper
