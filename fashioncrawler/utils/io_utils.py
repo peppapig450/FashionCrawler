a68900e8cbbbb75d09f34bb4b251777918d61601
peppapig450 <nbgenius1@gmail.com>
2024-04-19 12:18:53 -0700

    fix the path

--- fashioncrawler/utils/io_utils.py
+++ fashioncrawler/utils/io_utils.py
@@ -123,7 +123,7 @@ class IOUtils:
         args = parser.parse_args()
 
         # Load configuration from YAML file
-        config = IOUtils._load_config("../resources/config/config.yaml")
+        config = IOUtils._load_config("fashioncrawler/resources/config/config.yaml")
 
         # update config based on command line args
         if args.enable_site:
@@ -149,6 +149,7 @@ class IOUtils:
             dict: Configuration settings loaded from the file.
         """
         config_file = os.path.abspath(config_file)
+        print(config_file)
         try:
             with open(config_file, "r", encoding="UTF-8") as f:
                 config = yaml.safe_load(f)
